2f901f037bf3f7cf9518b7d1d22ae0c84c071a05
Oluwaseun Agboola <ogeariyo@gmail.com>
2023-01-31 09:13:50 +0100

    Initial commit

---
+++ src/main/java/org/example/jpa_recipe_project/JpaRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.jpa_recipe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jpa_recipe_project/JpaRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpa_recipe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
