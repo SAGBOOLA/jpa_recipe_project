ad25c2538b4695c41bb1b1030b493832f117f047
Oluwaseun Agboola <ogeariyo@gmail.com>
2023-02-02 23:56:34 +0100

    tests

--- src/main/java/org/example/jpa_recipe_project/repository/RecipeRepository.java
+++ src/main/java/org/example/jpa_recipe_project/repository/RecipeRepository.java
@@ -14,12 +14,18 @@ public interface RecipeRepository extends CrudRepository<Recipe, Integer>{
 
     List<Recipe> findAllByRecipeIngredients_Ingredient_IngredientNameContainsIgnoreCase(String ingredientName);
 
+    @Query("select r from Recipe r where r.recipeName like concat('%', :name, '%') ")
+    List<Recipe> findRecipeNameContain(@Param("name") String name);
 
-    @Query("select r from Recipe r where r.categories =:c")
-    List<Recipe> selectByCategory(@Param("c") String category);
+    @Query("select r from Recipe r, RecipeIngredient ri, Ingredient i " +
+            "where i = ri.ingredient and ri member of r.recipeIngredients and i = :name")
+    List<Recipe> selectRecipesByIngredientName(@Param("name") String ingredientName);
 
-    @Query("select r from Recipe r where r.categories = ?1 or r.categories = ?2 ")
-    List<Recipe> selectAllByRecipeThatMatchOneOrMoreCategories(@Param("1") String category1, @Param("2") String category2);
+    @Query("select r from Recipe r inner join r.categories c where c.category = :name")
+    List<Recipe> selectByCategory(@Param("name") String category);
+
+    @Query("select r from Recipe r inner join r.categories c where c.category in :names")
+    List<Recipe> selectAllByRecipeThatMatchOneOrMoreCategories(@Param("names") List<String> categories);
 
 
 }
